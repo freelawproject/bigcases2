a9340033ee00d8f325ad3ebe5979adc51f9e5618
Ansel Halliburton <github@anseljh.com>
2022-11-29 23:30:24 -0800

    Add a Docker image for Postgres

---
+++ database/bigcases2-ddl.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "case" (
+    id integer PRIMARY KEY,
+    court character varying(100) NOT NULL,
+    case_number character varying(100) NOT NULL,
+    bcb1_description character varying(100),
+    cl_case_title text,
+    cl_docket_id integer,
+    in_bcb1 boolean DEFAULT FALSE
+);
