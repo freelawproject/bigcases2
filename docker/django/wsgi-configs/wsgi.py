3608f3f1db7205d09e404075371695e12c96fb01
Eduardo Rosendo <eduardojra96@gmail.com>
2023-01-23 15:36:21 -0400

    feat(docker): Dockerize app
    - This commits removes the old dockerfile because it only contains commands to create the database and adds a new dockerfile to build the django image and the docker-compose file to define other services needed to develop the app.

---
+++ docker/django/wsgi-configs/wsgi.py
@@ -0,0 +1,17 @@
+"""
+WSGI config for bc project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bc.settings")
+
+sys.path.append("/opt/bigcases2")
+application = get_wsgi_application()
