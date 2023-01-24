1c5755e54eb6e3fe62d720e6bbe97a3a4cb7b8d7
Eduardo Rosendo <eduardojra96@gmail.com>
2023-01-24 00:46:40 -0400

    feat(cases): Add command to subscribe to a CL docket

---
+++ bc/cases/management/commands/subscribe.py
@@ -0,0 +1,17 @@
+import click
+from django.core.management.base import BaseCommand
+
+from bc.cases.utils.courtlistener import subscribe_to_docket_alert
+
+
+class Command(BaseCommand):
+    help = "Add a CourtListener docket alert subscription"
+
+    def add_arguments(self, parser):
+        parser.add_argument("cl_id", type=str)
+
+    def handle(self, *args, **options):
+        cl_id = options.get("cl_id", None)
+        if cl_id:
+            result = subscribe_to_docket_alert(cl_id)
+            click.echo(result)
