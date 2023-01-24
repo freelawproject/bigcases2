0fe328e55cc763d7c1c301c575fb2174955f9aa7
Eduardo Rosendo <eduardojra96@gmail.com>
2023-01-24 11:19:49 -0400

    feat(channels): Add command to subscribe to Mastodon push notifications

---
+++ bc/channels/management/commands/mastodon-subscribe.py
@@ -0,0 +1,29 @@
+import click
+from django.core.management.base import BaseCommand
+from mastodon import MastodonNotFoundError
+
+from bc.channels.utils.masto import get_mastodon, subscribe
+
+
+class Command(BaseCommand):
+    help = "Subscribe to Mastodon mention push notifications."
+
+    def handle(self, *args, **options):
+        m = get_mastodon()
+        sub = None
+
+        # Check if there's a subscription already
+        try:
+            sub = m.push_subscription()
+            click.echo(f"Got an existing subscription: {sub}")
+        except MastodonNotFoundError as e:
+            click.echo(e)
+
+            # Add subscription
+            sub = subscribe()
+
+        sub_id = sub["id"]
+        sub_key = sub["server_key"]
+        click.echo(f"Push subscription ID: {sub_id}. Key: {sub_key}")
+
+        click.echo("mastodon_subscribe_command() done.")
