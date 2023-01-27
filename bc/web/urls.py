9b4016058c3ff872860d99bd0bdf2315ac646b92
Eduardo Rosendo <eduardojra96@gmail.com>
2023-01-27 18:35:45 -0400

    fix(web): Change the pattern of the docket details URL

--- bc/web/urls.py
+++ bc/web/urls.py
@@ -5,5 +5,5 @@ from .views import count_dockets, view_docket
 urlpatterns = [
     path("", count_dockets, name="homepage"),
     # Docket pages
-    path("docket/<int:docket_id>/", view_docket, name="docket_details"),
+    path("docket/<int:subscription_id>/", view_docket, name="docket_details"),
 ]
