f03e4b0963f8a92e792591a9239148e5eee20007
Eduardo Rosendo <eduardojra96@gmail.com>
2023-01-27 16:35:10 -0400

    feat(subscription): Update the method to create a subscription from a docket
    - This commit add the logic to get the court name before saving the subscription.

--- bc/subscription/services.py
+++ bc/subscription/services.py
@@ -1,3 +1,5 @@
+from courts_db import find_court_by_id
+
 from .models import Subscription
 from .selectors import get_subscription_by_case_id
 
@@ -7,6 +9,11 @@ def create_subscription_from_docket(docket):
     Creates a new Subscription record for a given Docket from the CL API.
     Call get_case_from_cl() to get a Docket first.
     """
+    pacer_case_id = docket["pacer_case_id"]
+    case_result = get_subscription_by_case_id(case_id=pacer_case_id)
+    if case_result:
+        return case_result
+
     docket_name = docket["case_name"]
     docket_number = docket["docket_number"]
 
@@ -14,12 +21,9 @@ def create_subscription_from_docket(docket):
     cl_court_id = docket["court_id"]
 
     pacer_court_id = docket["court_id"]
-    pacer_case_id = docket["pacer_case_id"]
 
-    case_result = get_subscription_by_case_id(case_id=pacer_case_id)
-
-    if case_result:
-        return case_result
+    court = find_court_by_id(cl_court_id)
+    court_name = court[0]["name"] if len(court) == 1 else ''
 
     subscription = Subscription.objects.create(
         docket_name=docket_name,
@@ -28,6 +32,7 @@ def create_subscription_from_docket(docket):
         cl_court_id=cl_court_id,
         pacer_court_id=pacer_court_id,
         pacer_case_id=pacer_case_id,
+        court_name=court_name
     )
 
     return subscription
