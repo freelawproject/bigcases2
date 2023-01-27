627a8e5f66b00f38f5c6a5a17733c215a633d424
Eduardo Rosendo <eduardojra96@gmail.com>
2023-01-27 16:38:43 -0400

    refactor(web): Clean up views.py from the "web" app

--- bc/web/views.py
+++ bc/web/views.py
@@ -1,4 +1,3 @@
-from courts_db import find_court_by_id
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404
 from django.template.response import TemplateResponse
@@ -6,24 +5,16 @@ from django.template.response import TemplateResponse
 from bc.subscription.models import Subscription
 
 
-def view_docket(request: HttpRequest, docket_id: int) -> HttpResponse:
-    docket = get_object_or_404(Subscription, pk=docket_id)
-
-    court_name = None
-    court_results = find_court_by_id(docket.court)
-    if len(court_results) == 1:
-        court_name = court_results[0]["name"]
-
-    context = {
-        "docket": docket,
-        "court_name": court_name,
-    }
-    return TemplateResponse(request, "docket.html", context)
+def view_docket(request: HttpRequest, subscription_id: int) -> HttpResponse:
+    subscription = get_object_or_404(Subscription, pk=subscription_id)
+    return TemplateResponse(
+        request, "docket.html", {"subscription": subscription}
+    )
 
 
 def count_dockets(request: HttpRequest) -> HttpResponse:
-    dockets = Subscription.objects.count()
+    subscription_count = Subscription.objects.count()
 
-    context = {"dockets_count": dockets}
-
-    return TemplateResponse(request, "homepage.html", context)
+    return TemplateResponse(
+        request, "homepage.html", {"subscription_count": subscription_count}
+    )
