1a1825f9c0bd1661662c7c3ad51dd9b2d3d3dd03
Eduardo Rosendo <eduardojra96@gmail.com>
2023-01-27 15:40:23 -0400

    refactor(subscription): Clean up helpers functions related to CourtListener

--- bc/subscription/utils/courtlistener.py
+++ bc/subscription/utils/courtlistener.py
@@ -1,8 +1,4 @@
-import json
 import logging
-import re
-from pprint import pformat
-from typing import Union
 
 import courts_db
 import requests
@@ -11,7 +7,6 @@ from django.conf import settings
 from .exceptions import MultiDefendantCaseError
 
 logger = logging.getLogger(__name__)
-
 
 API_ROOT = "https://www.courtlistener.com/api/rest/v3"
 ENDPOINT_DOCKET_ALERTS = f"{API_ROOT}/docket-alerts/"
@@ -34,57 +29,43 @@ def lookup_court(court: str):
         return None
 
 
-def trim_docket_ending_number(s: str) -> str:
-    """
-    Trims weird numeric endings from BCB1 case numbers, like
-    "-12" from "1:18-cr-00215-12"
-    """
-    ending_pattern = re.compile(r"\d+(-\d+)$")
-    match = ending_pattern.search(s)
-    if match:
-        ending = match.groups()[0]
-        length = len(ending)
-        ret = s[:-length]
-        return ret
-    else:
-        return s
-
-
 def auth_header() -> dict:
     token = settings.COURTLISTENER_API_KEY
     header_dict = {"Authorization": f"Token {token}"}
     return header_dict
 
 
-def court_url_to_key(url: str) -> str:
-    """
-    Converts a court URL, which is returned by the CourtListner API,
-    into the short version.
-    Example: "https://www.courtlistener.com/api/rest/v3/courts/vawd/"
-             converts to "vawd"
-    """
-    tokens = url.split("/")
-    key = tokens[-2]
-    return key
-
-
 def lookup_docket_by_cl_id(cl_id: int):
+    """
+    Performs a GET query on /api/rest/v3/dockets/
+    to get a Docket using the CourtListener ID
+    """
     url = f"{API_ROOT}/dockets/{cl_id}/"
-    response = requests.get(url, headers=auth_header())
+    response = requests.get(url, headers=auth_header(), timeout=5)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+
+def lookup_docket_by_case_number(court: str, docket_number: str):
+    """
+    Performs a GET query on /api/rest/v3/dockets/
+    using the court_id and docket_number to get a
+    Docket.
+    """
+
+    url = f"{API_ROOT}/dockets/"
+    response = requests.get(
+        url,
+        params={"court_id": court, "docket_number": docket_number},
+        headers=auth_header(),
+        timeout=5,
+    )
     data = response.json()
-    return data
-
-
-def get_case_from_cl(court: str, case_number: str):
-    url = f"{API_ROOT}/dockets/?court__id={court}&docket_number={case_number}"
-    response = requests.get(url, headers=auth_header())
-    data = response.json()
-    ret = None
-
     num_results = data["count"]
-    print(data["count"])
     if num_results == 1:
-        ret = data["results"][0]
+        return data["results"][0]
     elif num_results == 0:
         return None
     else:
@@ -121,24 +102,6 @@ def get_case_from_cl(court: str, case_number: str):
 
         # TODO: Figure out how to choose the "best" of multiple dockets
 
-    return ret
-
-
-def get_docket_alert_subscriptions() -> list:
-    """
-    Performs a GET query on /api/rest/v3/docket-alerts/
-    to get a list of CourtListener docket IDs to which
-    we are subscribed.
-    """
-    response = requests.get(ENDPOINT_DOCKET_ALERTS, headers=auth_header())
-    response_data = response.json()
-    if response_data.get("count") == 0:
-        return []
-    filtered = [
-        r for r in response_data["results"] if r.get("alert_type") == 1
-    ]
-    return filtered
-
 
 def subscribe_to_docket_alert(cl_id: int) -> bool:
     """
@@ -151,6 +114,7 @@ def subscribe_to_docket_alert(cl_id: int) -> bool:
         data={
             "docket": cl_id,
         },
+        timeout=5,
     )
 
     if response.status_code == 201:
@@ -160,24 +124,6 @@ def subscribe_to_docket_alert(cl_id: int) -> bool:
             f"Error subscribing to case {cl_id}: got HTTP response {response.status_code}"
         )
         return False
-
-
-def lookup_judge(judge_resource: str):
-    """
-    Query CL and return information about a judge.
-    Input: judge_resource: a CL People resource URI like
-    "https://www.courtlistener.com/api/rest/v3/people/664/"
-    """
-    response = requests.get(
-        judge_resource,
-        headers=auth_header(),
-    )
-    print(
-        f"lookup_judge(): <{response.status_code}> for query to {judge_resource}"
-    )
-    response_data = response.json()
-    print(pformat(response_data))
-    return response_data
 
 
 def handle_multi_defendant_cases(queue):
@@ -190,14 +136,3 @@ def handle_multi_defendant_cases(queue):
         # TODO
         raise NotImplementedError
     logger.debug("handle_multi_defendant_cases(): done")
-
-
-def add_case(
-    court: str,
-    case_number: str,
-    name: str,
-    bcb1_name: Union[str, None] = None,
-    cl_id: Union[int, None] = None,
-    in_bcb1: bool = False,
-):
-    raise NotImplementedError
