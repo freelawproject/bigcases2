da5b0e69481629d07f6fed3a15508ac262a3a724
Eduardo Rosendo <eduardojra96@gmail.com>
2023-01-30 11:16:06 -0400

    feat(setting): Create a new module called settings
    - This commit creates a module called 'setting' to group the code related to the project's configuration

---
+++ bc/settings/__init__.py
@@ -0,0 +1,2 @@
+from .django import *
+from .third_party.sentry import *
--- bc/settings/django.py
+++ bc/settings/django.py
@@ -5,7 +5,7 @@ import environ
 env = environ.FileAwareEnv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parents[2]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
