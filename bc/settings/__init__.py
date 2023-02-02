915200893a59c2a4ce8529c1043eeabec7855fcb
Michael Lissner <mlissner@michaeljaylissner.com>
2023-02-01 17:44:55 -0800

    fix(settings): Import the new settings

--- bc/settings/__init__.py
+++ bc/settings/__init__.py
@@ -1,3 +1,6 @@
 from .django import *
+from .project.security import *
+from .project.testing import *
+from .third_party.courtlistener import *
 from .third_party.mastodon import *
 from .third_party.sentry import *
