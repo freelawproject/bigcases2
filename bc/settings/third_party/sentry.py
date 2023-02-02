7b880f57e720709432f4a42846450092ab9f7f70
Michael Lissner <mlissner@michaeljaylissner.com>
2023-02-01 17:41:02 -0800

    fix(settings): Remove IA-related cruft

--- bc/settings/third_party/sentry.py
+++ bc/settings/third_party/sentry.py
@@ -7,9 +7,6 @@ env = environ.FileAwareEnv()
 SENTRY_DSN = env("SENTRY_DSN", default="")
 SENTRY_SAMPLE_TRACE = env("SENTRY_SAMPLE_TRACE", default=1.0)
 
-# IA's library logs a lot of errors, which get sent to sentry unnecessarily
-ignore_logger("internetarchive.session")
-ignore_logger("internetarchive.item")
 
 if SENTRY_DSN:
     sentry_sdk.init(
