95eaf5fce31c39da8d6f05a11777f77bde261d13
Eduardo Rosendo <eduardojra96@gmail.com>
2023-02-02 16:32:26 -0400

    feat(rq): Add link in the Admin dashboard to monitor the status of the queues

--- bc/settings/third_party/rq.py
+++ bc/settings/third_party/rq.py
@@ -1,9 +1,10 @@
 from .redis import REDIS_DATABASES, REDIS_HOST, REDIS_PORT
 
+RQ_SHOW_ADMIN_LINK = True
+
 RQ_QUEUES = {
     "default": {
-        "HOST": REDIS_HOST,
-        "PORT": REDIS_PORT,
+        "URL": f"{REDIS_HOST}:{REDIS_PORT}",
         "DB": REDIS_DATABASES["QUEUE"],
     },
 }
