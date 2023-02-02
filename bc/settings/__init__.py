badb0f440c37aa084ce8d48a7606911845e8abd4
Eduardo Rosendo <eduardojra96@gmail.com>
2023-02-02 11:40:12 -0400

    feat(settings): Add rq settings

--- bc/settings/__init__.py
+++ bc/settings/__init__.py
@@ -4,4 +4,5 @@ from .project.testing import *
 from .third_party.courtlistener import *
 from .third_party.mastodon import *
 from .third_party.redis import *
+from .third_party.rq import *
 from .third_party.sentry import *
---
+++ bc/settings/third_party/rq.py
@@ -0,0 +1,9 @@
+from .redis import REDIS_DATABASES, REDIS_HOST, REDIS_PORT
+
+RQ_QUEUES = {
+    'default': {
+        'HOST': REDIS_HOST,
+        'PORT': REDIS_PORT,
+        'DB': REDIS_DATABASES['QUEUE'],
+    },
+}
