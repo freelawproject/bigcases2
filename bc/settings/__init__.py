8fc044a5b82f72016d44d2d92c933b065867eda7
Eduardo Rosendo <eduardojra96@gmail.com>
2023-02-02 13:26:30 -0400

    style: Change code styling using black

--- bc/settings/__init__.py
+++ bc/settings/__init__.py
@@ -3,7 +3,7 @@ from .project.security import *
 from .project.testing import *
 from .third_party.courtlistener import *
 from .third_party.mastodon import *
+from .third_party.pacer import *
 from .third_party.redis import *
 from .third_party.rq import *
-from .third_party.pacer import *
 from .third_party.sentry import *
