9cf81877562fc6cd669b1af668f17b2e07558ef1
Eduardo Rosendo <eduardojra96@gmail.com>
2023-02-07 23:41:19 -0400

    fix(subscribe): Add CloudFront header to check IP address

---
+++ bc/core/utils/network.py
@@ -0,0 +1,23 @@
+from typing import Optional
+
+
+def strip_port_to_make_ip_key(ip_str: Optional[str]) -> Optional[str]:
+    """Make a good key to use for caching the request's IP
+
+    CloudFront provides a header that returns the user's IP and port,
+    so we need to strip it to make the user's IP  a consistent key.
+
+    So we go from something like:
+
+        96.23.39.106:51396
+
+    To:
+
+        96.23.39.106
+
+    :param ip_str: the IP address of the viewer and the source port of the request
+    :return: A simple key that can be used to throttle the user if needed.
+    """
+    if ip_str:
+        return ip_str.split(":")[0]
+    return None
--- bc/subscription/api_permissions.py
+++ bc/subscription/api_permissions.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from rest_framework import exceptions, permissions
 
+from bc.core.utils.network import strip_port_to_make_ip_key
+
 
 class AllowListPermission(permissions.BasePermission):
     """
@@ -8,7 +10,12 @@ class AllowListPermission(permissions.BasePermission):
     """
 
     def has_permission(self, request, view):
-        ip_addr = request.META["REMOTE_ADDR"]
+        viewer_address = request.META["HTTP_CLOUDFRONT_VIEWER_ADDRESS"]
+        ip_addr = (
+            strip_port_to_make_ip_key(viewer_address)
+            or request.META["REMOTE_ADDR"]
+        )
+
         if settings.DEVELOPMENT:
             return True
         if ip_addr not in settings.COURTLISTENER_ALLOW_IPS:
