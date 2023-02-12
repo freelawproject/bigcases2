122e87c3ecd98d32b7845b3d4dede16a12ea3773
Eduardo Rosendo <eduardojra96@gmail.com>
2023-02-12 01:56:36 -0400

    feat(subscription): Update the task that processes webhooks
    - This commit adds new logic to post statuses to mastodon after a filling webhook has been processed

--- bc/subscription/tasks.py
+++ bc/subscription/tasks.py
@@ -1,4 +1,8 @@
-from .models import FilingWebhookEvent, Subscription
+from bc.channel.models import Channel
+from bc.channel.utils.masto import post_status
+from bc.core.utils.messages import NEW_FILING_TEMPLATE
+
+from .models import FilingWebhookEvent, Post, Subscription
 
 
 def process_filing_webhook_event(fwe_pk) -> FilingWebhookEvent:
@@ -15,6 +19,22 @@ def process_filing_webhook_event(fwe_pk) -> FilingWebhookEvent:
             )
             filing_webhook_event.subscription = subscription
             filing_webhook_event.save()
+
+            message = NEW_FILING_TEMPLATE.format(
+                docket=subscription.docket_name,
+                desc=filing_webhook_event.description,
+                link=filing_webhook_event.document_link(),
+            )
+
+            api_post_id = post_status(message)
+
+            channel = Channel.objects.get(service=Channel.MASTODON)
+            Post.objects.create(
+                filing_webhook_event=filing_webhook_event,
+                channel=channel,
+                object_id=api_post_id,
+            )
+
     except Subscription.DoesNotExist:
         filing_webhook_event.status = FilingWebhookEvent.FAILED
         filing_webhook_event.save()
