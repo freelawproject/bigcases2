f212b3b857b821890a5ae2a7e6022ffd6241131e
Eduardo Rosendo <eduardojra96@gmail.com>
2023-02-13 22:05:15 -0400

    refactor(core): Update the mastodon post template

--- bc/core/utils/messages.py
+++ bc/core/utils/messages.py
@@ -1,3 +1,4 @@
-NEW_FILING_TEMPLATE = (
-    "New filing in {docket}: {desc} \n\nDoc. on CourtListener: {link}"
-)
+MASTODON_POST_TEMPLATE = """New filing in {docket}: {desc}
+
+PDF: {pdf_link}
+Docket: {docket_link}"""
