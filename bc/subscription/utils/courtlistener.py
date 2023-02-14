837c7b46aacd39ca534b240e6367326b9a0c3580
Eduardo Rosendo <eduardojra96@gmail.com>
2023-02-13 23:38:57 -0400

    feat(subscription): Add helper methods to compute pacer_court_id

--- bc/subscription/utils/courtlistener.py
+++ bc/subscription/utils/courtlistener.py
@@ -13,6 +13,25 @@ CL_API = {
     "docket-alerts": "https://www.courtlistener.com/api/rest/v3/docket-alerts/",
 }
 
+pacer_to_cl_ids = {
+    # Maps PACER ids to their CL equivalents
+    "azb": "arb",  # Arizona Bankruptcy Court
+    "cofc": "uscfc",  # Court of Federal Claims
+    "neb": "nebraskab",  # Nebraska Bankruptcy
+    "nysb-mega": "nysb",  # Remove the mega thing
+}
+
+# Reverse dict of pacer_to_cl_ids
+cl_to_pacer_ids = {v: k for k, v in pacer_to_cl_ids.items() if v != "nysb"}
+
+
+def map_pacer_to_cl_id(pacer_id):
+    return pacer_to_cl_ids.get(pacer_id, pacer_id)
+
+
+def map_cl_to_pacer_id(cl_id):
+    return cl_to_pacer_ids.get(cl_id, cl_id)
+
 
 def lookup_court(court: str):
     """
