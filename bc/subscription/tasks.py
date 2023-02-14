e61748a3ddb1eaf35addad2ff9c779ec5f4e388f
Eduardo Rosendo <eduardojra96@gmail.com>
2023-02-13 22:13:27 -0400

    refactor(subscription): Refactor the file tasks.py
    - This commit updates the imports in this file
    - The new template for mastodon post is included in the method called process_filing_webhook_event
    - This commit uses the new field of the Post model to store the content of the post.

--- bc/subscription/tasks.py
+++ bc/subscription/tasks.py
@@ -1,8 +1,9 @@
-from bc.channel.models import Channel
-from bc.channel.utils.masto import post_status
-from bc.core.utils.messages import NEW_FILING_TEMPLATE
+from bc.channel.models import Post
+from bc.channel.selectors import get_mastodon_channel
+from bc.channel.utils.masto import post_status as mastodon_post
+from bc.core.utils.messages import MASTODON_POST_TEMPLATE
 
-from .models import FilingWebhookEvent, Post, Subscription
+from .models import FilingWebhookEvent, Subscription
 
 
 def process_filing_webhook_event(fwe_pk) -> FilingWebhookEvent:
@@ -12,31 +13,34 @@ def process_filing_webhook_event(fwe_pk) -> FilingWebhookEvent:
     :return: A FilingWebhookEvent object that was updated.
     """
     filing_webhook_event = FilingWebhookEvent.objects.get(pk=fwe_pk)
-    try:
-        if filing_webhook_event.docket_id:
+    if filing_webhook_event.docket_id:
+        try:
             subscription = Subscription.objects.get(
                 cl_docket_id=filing_webhook_event.docket_id
             )
-            filing_webhook_event.subscription = subscription
+        except Subscription.DoesNotExist:
+            filing_webhook_event.status = FilingWebhookEvent.FAILED
             filing_webhook_event.save()
+            return filing_webhook_event
 
-            message = NEW_FILING_TEMPLATE.format(
-                docket=subscription.docket_name,
-                desc=filing_webhook_event.description,
-                link=filing_webhook_event.document_link(),
-            )
-
-            api_post_id = post_status(message)
-
-            channel = Channel.objects.get(service=Channel.MASTODON)
-            Post.objects.create(
-                filing_webhook_event=filing_webhook_event,
-                channel=channel,
-                object_id=api_post_id,
-            )
-
-    except Subscription.DoesNotExist:
-        filing_webhook_event.status = FilingWebhookEvent.FAILED
+        filing_webhook_event.subscription = subscription
         filing_webhook_event.save()
+
+        message = MASTODON_POST_TEMPLATE.format(
+            docket=subscription.docket_name,
+            desc=filing_webhook_event.description,
+            pdf_link=filing_webhook_event.cl_pdf_or_pacer_url,
+            docket_link=filing_webhook_event.cl_docket_url,
+        )
+
+        api_post_id = mastodon_post(message)
+
+        channel = get_mastodon_channel()
+        Post.objects.create(
+            filing_webhook_event=filing_webhook_event,
+            channel=channel,
+            object_id=api_post_id,
+            text=message,
+        )
 
     return filing_webhook_event
