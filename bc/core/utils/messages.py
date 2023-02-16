c0e4457dd2acda41dac3626ef57834d87fe9c1c2
Eduardo Rosendo <eduardojra96@gmail.com>
2023-02-16 09:43:35 -0400

    doc(core): Add doctring to the template dataclass

--- bc/core/utils/messages.py
+++ bc/core/utils/messages.py
@@ -8,6 +8,11 @@ class MastodonTemplate:
     str_template: str
 
     def __len__(self):
+        """Returns the length of the template without the placeholders
+
+        All links count as 23 characters in mastodon no matter how long
+        they really are.
+        """
         return self.number_of_links * 23 + self.character_count
 
 
