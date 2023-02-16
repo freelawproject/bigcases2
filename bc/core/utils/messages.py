eae3c24c09631f4f4583fd7405bc6ac708e39868
Eduardo Rosendo <eduardojra96@gmail.com>
2023-02-16 08:18:11 -0400

    feat(core): Add a dataclass to represent mastodon templates

--- bc/core/utils/messages.py
+++ bc/core/utils/messages.py
@@ -1,4 +1,21 @@
-MASTODON_POST_TEMPLATE = """New filing in {docket}: {desc}
+from dataclasses import dataclass
+
+
+@dataclass
+class MastodonTemplate:
+    number_of_links: int
+    character_count: int
+    str_template: str
+
+    def __len__(self):
+        return self.number_of_links * 23 + self.character_count
+
+
+POST_TEMPLATE = MastodonTemplate(
+    number_of_links=2,
+    character_count=32,
+    str_template="""New filing in {docket}: {desc}
 
 PDF: {pdf_link}
-Docket: {docket_link}"""
+Docket: {docket_link}""",
+)
