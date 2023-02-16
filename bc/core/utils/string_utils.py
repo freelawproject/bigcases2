70fe578c43286da6a047a1370c69552160edbc8c
Eduardo Rosendo <eduardojra96@gmail.com>
2023-02-16 08:18:23 -0400

    feat(core): Add a helper function to truncate long strings

---
+++ bc/core/utils/string_utils.py
@@ -0,0 +1,29 @@
+def trunc(s: str, length: int, ellipsis: str | None = None) -> str:
+    """Truncates a string at a good length.
+
+    Finds the rightmost space in a string, and truncates there. Lacking such
+    a space, truncates at length.
+
+    If an ellipsis is provided, the right most space is used that allows the
+    addition of the ellipsis without being longer than length.
+    """
+    if ellipsis:
+        ellipsis_length = len(ellipsis)
+    else:
+        ellipsis_length = 0
+
+    if len(s) <= length:
+        # Do not ellipsize if the item is not truncated.
+        return s
+    else:
+        # find the rightmost space using a zero-indexed (+1) length minus the
+        # length of the ellipsis.
+        rightmost_space_index = length - ellipsis_length + 1
+        end = s.rfind(" ", 0, rightmost_space_index)
+        if end == -1:
+            # no spaces found, just use max position
+            end = length - ellipsis_length
+        s = s[0:end]
+        if ellipsis:
+            s = f"{s}{ellipsis}"
+        return s
