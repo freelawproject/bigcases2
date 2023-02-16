33a5c6723cd4ece71fc4381c960a4409c4946b6c
Eduardo Rosendo <eduardojra96@gmail.com>
2023-02-16 13:43:46 -0400

    feat(core): Add methods to the MastodonTemplate class
    - This commit adds two new methods to the MastodonTemplate class. The first one is called _available_space and computes the number of characters available in the post. The other method is called format and handles the template formatting.

--- bc/core/utils/messages.py
+++ bc/core/utils/messages.py
@@ -1,11 +1,14 @@
 from dataclasses import dataclass
 
+from bc.core.utils.string_utils import trunc
+
 
 @dataclass
 class MastodonTemplate:
-    number_of_links: int
     character_count: int
     str_template: str
+    link_placeholders: list[str]
+    max_characters: int = 500
 
     def __len__(self):
         """Returns the length of the template without the placeholders
@@ -13,14 +16,38 @@ class MastodonTemplate:
         All links count as 23 characters in mastodon no matter how long
         they really are.
         """
-        return self.number_of_links * 23 + self.character_count
+        return 23 * len(self.link_placeholders) + self.character_count
+
+    def _available_space(self, *args, **kwargs) -> int:
+        excluded = self.link_placeholders.copy()
+        excluded.append("description")
+
+        placeholder_characters = sum(
+            [
+                len(str(val))
+                for key, val in kwargs.items()
+                if key not in excluded
+            ]
+        )
+
+        return self.max_characters - len(self) - placeholder_characters
+
+    def format(self, *args, **kwargs) -> str:
+        if "description" in kwargs:
+            available_space = self._available_space("description", **kwargs)
+            if len(kwargs["description"]) > available_space:
+                kwargs["description"] = trunc(
+                    kwargs["description"], available_space, "…"
+                )
+
+        return self.str_template.format(**kwargs)
 
 
 POST_TEMPLATE = MastodonTemplate(
-    number_of_links=2,
     character_count=38,
+    link_placeholders=["pdf_link", "docket_link"],
     str_template="""New filing in {docket}
-Doc {doc_num}: {desc}
+Doc #{doc_num}: {description}
 
 PDF: {pdf_link}
 Docket: {docket_link}""",
