cb19a115510573f421106f32884e63fa389b217c
Eduardo Rosendo <eduardojra96@gmail.com>
2023-02-17 14:25:36 -0400

    feat(subscription): Add new argument to the lookup command

--- bc/subscription/management/commands/lookup.py
+++ bc/subscription/management/commands/lookup.py
@@ -16,6 +16,12 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument("cl-id", type=str)
         parser.add_argument(
+            "--name",
+            type=str,
+            help="Custom name for the case. This argument overwrites the name from CL",
+            required=False,
+        )
+        parser.add_argument(
             "--add",
             action="store_true",
             help="Save and Subscribe to the case found using the CL API",
@@ -23,30 +29,42 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         result = lookup_docket_by_cl_id(options["cl-id"])
-        if result:
-            court = result["court_id"]
-            case_name = result["case_name"]
-            docket_number = result["docket_number"]
-            uri = result["absolute_url"]
-            cl_url = f"https://www.courtlistener.com{uri}"
-            self.stdout.write(self.style.SUCCESS(f"Name: {case_name}"))
-            self.stdout.write(self.style.SUCCESS(f"Case no.: {docket_number}"))
-            self.stdout.write(self.style.SUCCESS(f"Court: {court}"))
-            self.stdout.write(self.style.SUCCESS(f"Link: {cl_url}"))
-
-            if options["add"]:
-                self.stdout.write(
-                    self.style.WARNING("We'll try to add this case to the DB.")
-                )
-                _, created = create_or_update_subscription_from_docket(result)
-                message = "Added!" if created else "Updated!"
-                self.stdout.write(self.style.SUCCESS(message))
-
-                if created:
-                    cl_subscription = subscribe_to_docket_alert(
-                        options["cl-id"]
-                    )
-                    if cl_subscription:
-                        self.stdout.write(self.style.SUCCESS("Subscribed!"))
-        else:
+        if not result:
             self.stdout.write(self.style.ERROR("Case not found"))
+            return
+
+        court = result["court_id"]
+        case_name = result["case_name"]
+        docket_number = result["docket_number"]
+        uri = result["absolute_url"]
+        cl_url = f"https://www.courtlistener.com{uri}"
+        self.stdout.write(self.style.SUCCESS(f"Name: {case_name}"))
+        self.stdout.write(self.style.SUCCESS(f"Case no.: {docket_number}"))
+        self.stdout.write(self.style.SUCCESS(f"Court: {court}"))
+        self.stdout.write(self.style.SUCCESS(f"Link: {cl_url}"))
+
+        if not options["add"]:
+            return
+
+        self.stdout.write(
+            self.style.WARNING("We'll try to add this case to the DB.")
+        )
+        custom_name = options.get("name")
+        if custom_name:
+            result["case_name"] = custom_name
+            self.stdout.write(
+                self.style.WARNING(
+                    f"We'll rename the case to: '{custom_name}'."
+                )
+            )
+
+        _, created = create_or_update_subscription_from_docket(result)
+        message = "Added!" if created else "Updated!"
+        self.stdout.write(self.style.SUCCESS(message))
+
+        if not created:
+            return
+
+        cl_subscription = subscribe_to_docket_alert(options["cl-id"])
+        if cl_subscription:
+            self.stdout.write(self.style.SUCCESS("Subscribed!"))
