d5835cbf8e07ff019174c2ed6271a578b232a7e7
Eduardo Rosendo <eduardojra96@gmail.com>
2023-03-09 22:40:17 -0400

    feat(web): Include Flowbite to the content list of the config file

--- bc/web/static_src/tailwind.config.js
+++ bc/web/static_src/tailwind.config.js
@@ -36,7 +36,8 @@ module.exports = {
         // '!../../**/node_modules',
         /* JS 2: Process all JavaScript files in the project. */
         // '../../**/*.js',
-
+        "./node_modules/flowbite/**/*.js",
+        
         /**
          * Python: If you use Tailwind CSS classes in Python, uncomment the following line
          * and make sure the pattern below matches your project structure.
