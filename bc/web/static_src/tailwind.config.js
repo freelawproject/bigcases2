d1c74f3cc9bed166a1b93da7c84696aabc706884
Eduardo Rosendo <eduardojra96@gmail.com>
2023-03-09 22:31:47 -0400

    feat(web): Require Flowbite as a plugin in the tailwind.config.js file

--- bc/web/static_src/tailwind.config.js
+++ bc/web/static_src/tailwind.config.js
@@ -76,5 +76,6 @@ module.exports = {
         require('@tailwindcss/typography'),
         require('@tailwindcss/line-clamp'),
         require('@tailwindcss/aspect-ratio'),
+        require('flowbite/plugin')
     ],
 }
