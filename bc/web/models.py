449108782f7a5d2b629b7ed05ca24b83ec393e52
Eduardo Rosendo <eduardojra96@gmail.com>
2023-03-14 11:25:10 -0400

    feat(web): Add models to store data from from the forms

---
+++ bc/web/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+from bc.core.models import AbstractDateTimeModel
+
+
+class BotSuggestion(AbstractDateTimeModel):
+    name = models.CharField(
+        help_text="Name of the user making the suggestion",
+        max_length=100,
+    )
+    platform = models.TextField(
+        help_text="List of platforms where the bot would work"
+    )
+    use_case = models.TextField(
+        help_text="Description of bot and the kinds of cases it would follow."
+    )
+
+
+class WaitList(AbstractDateTimeModel):
+    platform = models.TextField(
+        help_text="List of platforms to include features of the bots"
+    )
+    name = models.CharField(
+        help_text="Name of the user submitting the form",
+        max_length=100,
+    )
+    email = models.EmailField(
+        help_text="Email of the user submitting the form",
+    )
+    company_name = models.TextField(blank=True)
+    
