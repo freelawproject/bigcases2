d1a5973267944bf4adeaae07979c7d67bbfa3068
Eduardo Rosendo <eduardojra96@gmail.com>
2023-03-17 16:05:42 -0400

    feat(urls): Redirect the admin login to the ratelimited version

--- bc/urls.py
+++ bc/urls.py
@@ -2,9 +2,16 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path(
+        # Redirect admin login to our ratelimited version.
+        "admin/login/",
+        RedirectView.as_view(url="/sign-in/", permanent=False),
+    ),
     path("admin/", admin.site.urls),
+    path("", include("bc.users.urls")),
     path("", include("bc.web.urls")),
     path("", include("bc.core.urls")),
     path("", include("bc.channel.urls")),
