4f38ad7f6129a6075a64260e77d3634a1a5832b3
Eduardo Rosendo <eduardojra96@gmail.com>
2023-03-22 13:07:51 -0400

    refactor(core): Move all the template instances to a file called templates.py

---
+++ bc/core/utils/status/templates.py
@@ -0,0 +1,49 @@
+import re
+
+from .base import TwitterTemplate, MastodonTemplate
+
+
+DO_NOT_POST = re.compile(
+    r"""(
+    pro\shac\svice|                 #pro hac vice
+    notice\sof\sappearance|         #notice of appearance
+    certificate\sof\sdisclosure|    #certificate of disclosure
+    corporate\sdisclosure|          #corporate disclosure
+    add\sand\sterminate\sattorneys| #add and terminate attorneys
+    none                            #entries with bad data
+    )""",
+    re.VERBOSE | re.IGNORECASE,
+)
+
+MASTODON_POST_TEMPLATE = MastodonTemplate(
+    link_placeholders=["pdf_link", "docket_link"],
+    str_template="""New filing: "{docket}"
+Doc #{doc_num}: {description}
+
+PDF: {pdf_link}
+Docket: {docket_link}""",
+)
+
+
+MASTODON_MINUTE_TEMPLATE = MastodonTemplate(
+    link_placeholders=["docket_link"],
+    str_template="""New minute entry in {docket}: {description}
+
+Docket: {docket_link}""",
+)
+
+TWITTER_POST_TEMPLATE = TwitterTemplate(
+    link_placeholders=["pdf_link", "docket_link"],
+    str_template="""New filing: "{docket}"
+Doc #{doc_num}: {description}
+
+PDF: {pdf_link}
+Docket: {docket_link}""",
+)
+
+TWITTER_MINUTE_TEMPLATE = TwitterTemplate(
+    link_placeholders=["docket_link"],
+    str_template="""New minute entry in {docket}: {description}
+
+Docket: {docket_link}""",
+)
