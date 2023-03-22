6addb657b0f9f7a9fc8be6232149ffd60821c80f
Eduardo Rosendo <eduardojra96@gmail.com>
2023-03-22 10:14:27 -0400

    feat(settings): Update variables to access Twitter API

--- bc/settings/third_party/twitter.py
+++ bc/settings/third_party/twitter.py
@@ -2,7 +2,7 @@ import environ
 
 env = environ.FileAwareEnv()
 
-TWITTER_API_KEY = env("TWITTER_API_KEY", default="")
-TWITTER_API_SECRET = env("TWITTER_API_SECRET", default="")
+TWITTER_CONSUMER_KEY = env("TWITTER_CONSUMER_KEY", default="")
+TWITTER_CONSUMER_SECRET = env("TWITTER_CONSUMER_SECRET", default="")
 TWITTER_ACCESS_TOKEN = env("TWITTER_ACCESS_TOKEN", default="")
 TWITTER_ACCESS_TOKEN_SECRET = env("TWITTER_ACCESS_TOKEN_SECRET", default="")
