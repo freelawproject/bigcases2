f9ebbc7d48e906944bf5750a1fe27e51d10e8249
Eduardo Rosendo <eduardojra96@gmail.com>
2023-04-04 19:26:03 -0400

    refactor(sponsorship): Tweak helper method that updates sponsorships
    This commit uses a F object to update the current_amount field of the sponsorship record related to the purchase and also adds a docstring to the method called update_sponsorships_current_amount.

--- bc/sponsorship/utils.py
+++ bc/sponsorship/utils.py
@@ -1,11 +1,22 @@
 from decimal import Decimal
 
-from .models import Sponsorship, Transaction
+from django.db.models import F
+
+from .models import Transaction
 
 
 def update_sponsorships_current_amount(ledger_entry: Transaction) -> None:
-    if not ledger_entry.sponsorship_id:
+    """
+    This method updates the current_amount field of the sponsorship record
+    related to the given ledger entry after a document has been purchased.
+
+    Args:
+        ledger_entry (Transaction): The transaction object related to the purchase.
+    """
+    if not ledger_entry.sponsorship:
         return None
-    sponsorship = Sponsorship.objects.get(pk=ledger_entry.sponsorship_id)
-    sponsorship.current_amount -= Decimal(ledger_entry.amount)
+    sponsorship = ledger_entry.sponsorship
+    sponsorship.current_amount = F("current_amount") - Decimal(
+        ledger_entry.amount
+    )
     sponsorship.save()
