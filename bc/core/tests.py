e06d724e2cb89634c4dd08cc35878dab79633447
Eduardo Rosendo <eduardojra96@gmail.com>
2023-04-10 16:59:49 -0400

    fix(core): Check title overflow in the TextImage class

--- bc/core/tests.py
+++ bc/core/tests.py
@@ -1,5 +1,7 @@
+from textwrap import wrap
 from unittest import TestCase
 
+from bc.core.utils.images import TextImage
 from bc.core.utils.status.templates import MastodonTemplate
 
 
@@ -32,7 +34,7 @@ class MastodonTemplateTest(TestCase):
             self.assertEqual(
                 result,
                 test["length"],
-                msg=("Failed with dict: %s.\n" "%s is longer than %s")
+                msg="Failed with dict: %s.\n%s is longer than %s"
                 % (test, result, test["length"]),
             )
 
@@ -73,7 +75,7 @@ class MastodonTemplateTest(TestCase):
             self.assertEqual(
                 result,
                 test["length"],
-                msg=("Failed with dict: %s.\n" "%s is longer than %s")
+                msg="Failed with dict: %s.\n%s is longer than %s"
                 % (test, result, test["length"]),
             )
 
@@ -94,7 +96,71 @@ class MastodonTemplateTest(TestCase):
             result, _ = template.format(**test)
             self.assertTrue(
                 len(result) <= template.max_characters,
-                msg="Failed with dict: %s.\n"
-                "%s is longer than %s"
+                msg="Failed with dict: %s.\n%s is longer than %s"
                 % (test, result, template.max_characters),
+            )
+
+
+class TextImageTest(TestCase):
+    tests = (
+        # Simple case
+        {"title": "short title", "description": 10 * "short description"},
+        # Short title and long description
+        {"title": 5 * "short title", "description": 20 * "short description"},
+        # Long title and long description
+        {"title": 15 * "short title", "description": 20 * "short description"},
+        {"title": 30 * "short title", "description": 50 * "short description"},
+    )
+
+    def test_creates_canvas_smaller_than_max_width(self):
+        for test in self.tests:
+            instance = TextImage(**test)
+            width, _ = instance.get_initial_dimensions()
+            self.assertLessEqual(
+                width,
+                instance.max_width,
+                msg="Failed with dict: %s.\n%s is larger than %s"
+                % (test, width, instance.max_width),
+            )
+
+    def test_max_character_count_avoids_overflow(self):
+        EDGE_CASE = (
+            {
+                "title": (
+                    "Case: Braidwood Management v. Becerra (ACA prev. care"
+                    " challenge)"
+                ),
+                "description": (
+                    "Transcript Order Form: re 115 Notice of Appeal,,,,"
+                    " transcript not requested Reminder: If the transcript is"
+                    " ordered for an appeal, Appellant must also file a copy"
+                    " of the order form with the appeals court. (Lynch,"
+                    " Christopher) (Entered: 04/10/2023)"
+                ),
+            },
+        )
+
+        for test in self.tests + EDGE_CASE:
+            instance = TextImage(**test)
+            instance.width, _ = instance.get_initial_dimensions()
+
+            # compute the max number of character to render in each line
+            max_character = instance.get_max_character_count()
+
+            # wrap the title and description using the max_character variable
+            wrapped_title = wrap(instance.title, max_character)
+            wrapped_desc = wrap(instance.description, max_character)
+
+            # check if the title overflows the canvas
+            self.assertGreaterEqual(
+                instance.get_available_space(wrapped_title),
+                0,
+                msg=f"Failed with dict: {test}.\nthe title overflows the image",
+            )
+
+            # check if the description overflows the canvas
+            self.assertGreaterEqual(
+                instance.get_available_space(wrapped_desc),
+                0,
+                msg=f"Failed with dict: {test}.\nthe description overflows the image",
             )
