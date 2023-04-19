f4a0054000c42d118c8ced09548f0d55f26c30b2
Eduardo Rosendo <eduardojra96@gmail.com>
2023-04-18 20:07:26 -0400

    feat(user): Enhance admin site for the User model

--- bc/users/admin.py
+++ bc/users/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from .models import User
 
-admin.site.register(User)
+
+class UserAdmin(BaseUserAdmin):
+    fieldsets = BaseUserAdmin.fieldsets + (  # type: ignore
+        # Custom fields added on to the bottom
+        ("Extra Fields", {"fields": ["affiliation"]}),
+    )
+    add_fieldsets = (
+        (None, {"classes": ["wide"], "fields": ["email"]}),
+    ) + BaseUserAdmin.add_fieldsets
+
+
+admin.site.register(User, UserAdmin)
