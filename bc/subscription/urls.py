eab3d090be2dd0b4c42292d24089dd460f271c20
Eduardo Rosendo <eduardojra96@gmail.com>
2023-04-24 10:55:00 -0400

    feat(subscription): Add new endpoints to the urls.py file

--- bc/subscription/urls.py
+++ bc/subscription/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from .api_views import handle_docket_alert_webhook, handle_recap_fetch_webhook
+from .views import search
 
 urlpatterns = [
     path(
@@ -12,5 +14,11 @@ urlpatterns = [
         "webhooks/recap-fetch/",
         handle_recap_fetch_webhook,
         name="recap_fetch_webhook_handler",
+    ),
+    path("search-case/", search, name="search_cases"),
+    path(
+        "add-cases/",
+        TemplateView.as_view(template_name="add-case.html"),
+        name="add_cases",
     ),
 ]
