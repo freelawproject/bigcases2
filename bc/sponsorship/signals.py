fcee74d7b9bc50f73adcc931d68023d14b0f76d8
Eduardo Rosendo <eduardojra96@gmail.com>
2023-04-25 12:41:02 -0400

    feat(sponsorship): Schedule a task to invalidate cloudfront cache

--- bc/sponsorship/signals.py
+++ bc/sponsorship/signals.py
@@ -1,9 +1,17 @@
+from django.conf import settings
 from django.db import transaction as db_transaction
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.urls import reverse
+from django_rq.queues import get_queue
+from rq import Retry
+
+from bc.core.utils.cloudfront import create_cache_invalidation
 
 from .models import Sponsorship, Transaction
 from .utils import update_sponsorships_current_amount
+
+queue = get_queue("default")
 
 
 @receiver(post_save, sender=Sponsorship)
@@ -17,6 +25,17 @@ def sponsorship_handler(sender, instance=None, created=False, **kwargs):
             sponsorship=instance,
             amount=instance.original_amount,
         )
+
+        # fires a job to create a new invalidation
+        if settings.AWS_CLOUDFRONT_DISTRIBUTION_ID:
+            queue.enqueue(
+                create_cache_invalidation,
+                reverse("big_cases_sponsors"),
+                retry=Retry(
+                    max=settings.RQ_MAX_NUMBER_OF_RETRIES,
+                    interval=settings.RQ_RETRY_INTERVAL,
+                ),
+            )
 
 
 @receiver(post_save, sender=Transaction)
