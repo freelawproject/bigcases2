15d726c9d9c880fce94e69008884597a8af9895b
Eduardo Rosendo <eduardojra96@gmail.com>
2023-04-25 12:40:36 -0400

    feat(sponsorship): Add the list of current and past sponsors

--- bc/sponsorship/selectors.py
+++ bc/sponsorship/selectors.py
@@ -1,3 +1,5 @@
+from collections.abc import Iterable
+
 from .models import Sponsorship
 
 
@@ -6,4 +8,20 @@ def get_active_sponsorship() -> Sponsorship | None:
         Sponsorship.objects.filter(current_amount__gte=3.00)
         .order_by("date_created")
         .first()
+    )
+
+
+def get_current_sponsor_organization() -> Iterable[Sponsorship]:
+    return (
+        Sponsorship.objects.filter(current_amount__gte=3.00)
+        .distinct("user_id")
+        .all()
+    )
+
+
+def get_past_sponsor_organization() -> Iterable[Sponsorship]:
+    return (
+        Sponsorship.objects.filter(current_amount__lte=3.00)
+        .distinct("user_id")
+        .all()
     )
