84095a0c68ae1a6f371ec35eb4c8a40a9e6d74c0
Eduardo Rosendo <eduardojra96@gmail.com>
2023-05-04 17:46:35 -0400

    feat(sponsorship): Add the aliasInline class to sponsorships.

--- bc/sponsorship/admin.py
+++ bc/sponsorship/admin.py
@@ -1,11 +1,21 @@
 from django.contrib import admin
 
+from bc.channel.models import Alias
+
 from .models import Sponsorship, Transaction
+
+
+class AliasInline(admin.TabularInline):
+    verbose_name = "Sponsored Alias"
+    verbose_name_plural = "Sponsored Aliases"
+    model = Alias.sponsorships.through
+    extra = 0
 
 
 @admin.register(Sponsorship)
 class SponsorshipAdmin(admin.ModelAdmin):
     list_display = ("user", "original_amount", "current_amount")
+    inlines = (AliasInline,)
     exclude = ("current_amount",)
 
 
