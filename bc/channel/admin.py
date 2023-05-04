717ad5f8274ff91e6d1a9c4d3efbe67fde9d2c3c
Eduardo Rosendo <eduardojra96@gmail.com>
2023-05-04 17:33:37 -0400

    feat(channel): Enhance the admin site for the channel and alias model
    This commit adds the userInline class to the admin interface for channels and also registers the Alias model to the admin site.

--- bc/channel/admin.py
+++ bc/channel/admin.py
@@ -1,5 +1,35 @@
 from django.contrib import admin
 
-from .models import Channel
+from .models import Alias, Channel
 
-admin.site.register(Channel)
+
+class UserInline(admin.StackedInline):
+    verbose_name = "Curator"
+    verbose_name_plural = "Curators"
+    model = Channel.user.through
+    extra = 0
+
+
+class ChannelAdmin(admin.ModelAdmin):
+    inlines = [
+        UserInline,
+    ]
+    exclude = ["user"]
+
+
+class ChannelInline(admin.TabularInline):
+    model = Channel
+    extra = 0
+    fields = ["service", "account", "account_id", "enabled"]
+
+    def has_add_permission(self, request, obj):
+        return False
+
+
+class AliasAdmin(admin.ModelAdmin):
+    inlines = (ChannelInline,)
+    exclude = ["sponsorships"]
+
+
+admin.site.register(Channel, ChannelAdmin)
+admin.site.register(Alias, AliasAdmin)
