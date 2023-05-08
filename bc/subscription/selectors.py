b9568bfe2c135e71b8118a6fe6619645e9555eb8
Eduardo Rosendo <eduardojra96@gmail.com>
2023-05-08 15:38:27 -0400

    feat(web): Add selector to filter subscriptions in the about page
    This commits adds a query to filter the list of subscriptions and show only those link to channels related to the Big Cases bot and not for the other new bots.

--- bc/subscription/selectors.py
+++ bc/subscription/selectors.py
@@ -1,7 +1,15 @@
-from typing import Optional
+from collections.abc import Iterable
 
 from .models import Subscription
 
 
 def get_subscription_by_case_id(case_id) -> Subscription | None:
     return Subscription.objects.filter(pacer_case_id=case_id).first()
+
+
+def get_subscriptions_for_big_cases() -> Iterable[Subscription]:
+    return (
+        Subscription.objects.filter(channel__group__is_big_cases=True)
+        .distinct("cl_docket_id")
+        .all()
+    )
