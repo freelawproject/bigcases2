a86db02e3ef965549ab1f261e24048b181ef7863
Eduardo Rosendo <eduardojra96@gmail.com>
2023-05-08 20:04:03 -0400

    feat(channel): Tweak the post command to use new helpers

--- bc/channel/management/commands/post.py
+++ bc/channel/management/commands/post.py
@@ -1,48 +1,82 @@
+from django.conf import settings
 from django.core.management.base import BaseCommand
-from prettytable import PrettyTable
+from django_rq.queues import get_queue
+from rq import Retry
 
-from bc.channel.models import Channel
-from bc.channel.utils.connectors.masto import MastodonConnector
-from bc.channel.utils.connectors.twitter import TwitterConnector
+from bc.channel.models import Channel, Group
+from bc.core.utils.commands import (
+    show_all_channels_table,
+    show_channel_groups_table,
+)
+
+queue = get_queue("default")
+
+
+def handle_post_command(
+    ids: list[int], mapping: dict[int, Channel | Group], text: str
+) -> None:
+    for record_id in ids:
+        record = mapping.get(record_id)
+
+        if record is None:
+            raise ValueError(f"No channel {record_id}")
+
+        if isinstance(record, Group):
+            for channel in record.channels.all():
+                api = channel.get_api_wrapper()
+                queue.enqueue(
+                    api.add_status,
+                    text,
+                    None,
+                    retry=Retry(
+                        max=settings.RQ_MAX_NUMBER_OF_RETRIES,
+                        interval=settings.RQ_RETRY_INTERVAL,
+                    ),
+                )
+        else:
+            api = record.get_api_wrapper()
+            queue.enqueue(
+                api.add_status,
+                text,
+                None,
+                retry=Retry(
+                    max=settings.RQ_MAX_NUMBER_OF_RETRIES,
+                    interval=settings.RQ_RETRY_INTERVAL,
+                ),
+            )
 
 
 class Command(BaseCommand):
     help = "Post something manually to one or more channels."
 
-    def handle(self, *args, **options):
-        tab = PrettyTable()
-        channel_mapping = {}
-        tab.field_names = ["ID", "Service", "Account", "Enabled", "URL"]
-        db_channels = Channel.objects.all()
-        for channel in db_channels:
-            tab.add_row(
-                [
-                    channel.id,
-                    channel.service,
-                    channel.account,
-                    channel.enabled,
-                    channel.self_url(),
-                ]
-            )
-            channel_mapping[channel.id] = channel
-        self.stdout.write(self.style.SUCCESS(tab))
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--show_groups",
+            action="store_true",
+            help="Shows the list of groups instead of individual channels",
+        )
 
-        channel_input = input(
-            "Which channels? Input ID, comma-separate for multiple, "
+    def handle(self, *args, **options):
+        if not options["show_groups"]:
+            table, mapping = show_all_channels_table()
+        else:
+            table, mapping = show_channel_groups_table()
+
+        self.stdout.write(self.style.SUCCESS(table))
+
+        instance = "group" if options["show_groups"] else "channel"
+
+        ch_input = input(
+            f"Which {instance}? Input ID, comma-separate for multiple, "
             "or 'all' for all of them.\n"
         )
-        self.stdout.write(self.style.SUCCESS(channel_input))
 
-        if channel_input == "all":
-            channel_ids = list(channel_mapping.keys())
+        if ch_input == "all":
+            input_ids = list(mapping.keys())
         else:
-            channel_ids = list(
-                map(int, [s.strip() for s in channel_input.split(",")])
+            input_ids = list(
+                map(int, [s.strip() for s in ch_input.split(",")])
             )
-
-        self.stdout.write(
-            self.style.SUCCESS(f"Posting to channels: {channel_ids}")
-        )
 
         post_text = input("What do you want to say? ")
 
@@ -52,31 +86,4 @@ class Command(BaseCommand):
             )
         )
 
-        for channel_id in channel_ids:
-            channel = channel_mapping.get(channel_id)
-            if channel is None:
-                raise ValueError(f"No channel {channel_id}")
-
-            match channel.service:
-                case Channel.MASTODON:
-                    self.stdout.write(
-                        self.style.SUCCESS(
-                            f"Let's toot from {channel.account}!"
-                        )
-                    )
-
-                    m = MastodonConnector()
-                    m.add_status(post_text)
-                case Channel.TWITTER:
-                    self.stdout.write(
-                        self.style.SUCCESS(
-                            f"Let's tweet from {channel.account}!"
-                        )
-                    )
-
-                    api = TwitterConnector()
-                    api.add_status(post_text)
-                case _:
-                    raise NotImplementedError(
-                        f"Not posting to {channel.service} yet"
-                    )
+        handle_post_command(input_ids, mapping, post_text)
