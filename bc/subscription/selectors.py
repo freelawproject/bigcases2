6468f0b2470a531e528ce6965b4b36575e1bf4b4
Eduardo Rosendo <eduardojra96@gmail.com>
2023-05-11 08:04:37 -0400

    feat(subscription): Tweak type hinting in the selector.py file

--- bc/subscription/selectors.py
+++ bc/subscription/selectors.py
@@ -1,4 +1,4 @@
-from collections.abc import Iterable
+from django.db.models import QuerySet
 
 from .models import Subscription
 
@@ -7,7 +7,13 @@ def get_subscription_by_case_id(case_id) -> Subscription | None:
     return Subscription.objects.filter(pacer_case_id=case_id).first()
 
 
-def get_subscriptions_for_big_cases() -> Iterable[Subscription]:
+def get_subscriptions_for_big_cases() -> QuerySet[Subscription]:
+    """
+    Returns the list of subscriptions for the Big Cases bot.
+
+    Returns:
+        QuerySet[Subscription]: list of subscriptions
+    """
     return (
         Subscription.objects.filter(channel__group__is_big_cases=True)
         .distinct("cl_docket_id")
