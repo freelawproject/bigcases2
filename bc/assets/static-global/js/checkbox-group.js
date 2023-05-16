b270646a7ed5f38bd8057135c5419708f690615a
Eduardo Rosendo <eduardojra96@gmail.com>
2023-05-16 15:41:08 -0400

    feat(template): Add buttons to interact with checkboxes

--- bc/assets/static-global/js/checkbox-group.js
+++ bc/assets/static-global/js/checkbox-group.js
@@ -1,10 +1,11 @@
 
 var CheckboxGroup =  (function () {
-  function CheckboxGroup(targetElement, triggerElement) {
+  function CheckboxGroup(targetElement, triggerElement, removeCheckElement) {
       if (targetElement === void 0) { targetElement = null; }
       if (triggerElement === void 0) { triggerElement = null; }
       this._targetEl = targetElement;
       this._triggerEl = triggerElement;
+      this._removeCheck = removeCheckElement;
       this._checked = true;
       this._init();
   }
@@ -16,7 +17,10 @@ var CheckboxGroup =  (function () {
   CheckboxGroup.prototype._setupEventListeners = function () {
     var _this = this;
     this._triggerEl.addEventListener('click', function () {
-        _this.toggle();
+      _this.check();
+    });
+    this._removeCheck.addEventListener('click', function () {
+      _this.uncheck();
     });
   };
   CheckboxGroup.prototype.toggle = function () {
@@ -48,9 +52,11 @@ function initCheckboxes() {
       .querySelectorAll('[data-cg-target]')
       .forEach(function ($triggerEl) {
       var targetClass = $triggerEl.getAttribute('data-cg-target');
+      var noneButtonId = $triggerEl.getAttribute('data-cg-uncheck');
+      var removeCheckButton = document.getElementById(noneButtonId)
       var $groupEl = document.querySelectorAll(`.${targetClass}`);
       if ($groupEl) {
-        new CheckboxGroup($groupEl, $triggerEl);
+        new CheckboxGroup($groupEl, $triggerEl, removeCheckButton);
       }
       else {
           console.error("The checkbox element with id \"".concat(targetClass, "\" does not exist. Please check the data-cg-toggle attribute."));
