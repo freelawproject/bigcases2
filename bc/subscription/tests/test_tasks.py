162cd2c3d57b04646f6a3b0379584f00a4081151
Eduardo Rosendo <eduardojra96@gmail.com>
2023-05-22 23:11:20 -0400

    feat(subscription): Add tests for the process_fetch_webhook method

--- bc/subscription/tests/test_tasks.py
+++ bc/subscription/tests/test_tasks.py
@@ -7,6 +7,7 @@ from bc.sponsorship.tests.factories import SponsorshipFactory
 from bc.subscription.models import FilingWebhookEvent
 from bc.subscription.tasks import (
     check_webhook_before_posting,
+    process_fetch_webhook_event,
     process_filing_webhook_event,
 )
 
@@ -56,9 +57,9 @@ class CheckWebhookBeforePostingTest(TestCase):
             status=FilingWebhookEvent.SUCCESSFUL,
         )
 
-    def raise_exception_for_record_wo_subscription(self, mock_lookup):
+    def test_raise_exception_for_webhook_no_subscription(self, mock_lookup):
         webhook_event_no_subscription = FilingWebhookEventFactory(
-            status=FilingWebhookEvent.SUCCESSFUL
+            subscription=None, status=FilingWebhookEvent.SUCCESSFUL
         )
         with self.assertRaises(AssertionError):
             check_webhook_before_posting(webhook_event_no_subscription.id)
@@ -124,3 +125,46 @@ class CheckWebhookBeforePostingTest(TestCase):
         )
         mock_purchase.assert_called_with(self.webhook_event.doc_id)
         mock_download.assert_not_called()
+
+
+class ProcessFetchWebhookEvent(TestCase):
+    webhook_event = None
+    webhook_no_subscription = None
+
+    @classmethod
+    def setUpTestData(cls) -> None:
+        subscription = SubscriptionFactory()
+        cls.webhook_no_subscription = FilingWebhookEventFactory(
+            subscription=None
+        )
+        cls.webhook_event = FilingWebhookEventFactory(
+            doc_id=2974081,
+            subscription=subscription,
+            status=FilingWebhookEvent.WAITING_FOR_DOCUMENT,
+        )
+
+    def test_raise_exception_for_webhook_no_subscription(self):
+        with self.assertRaises(AssertionError):
+            process_fetch_webhook_event(self.webhook_no_subscription.id)
+
+    @patch("bc.subscription.tasks.enqueue_posts_for_docket_alert")
+    @patch("bc.subscription.tasks.download_pdf_from_cl")
+    @patch("bc.subscription.tasks.lookup_document_by_doc_id")
+    def test_can_create_post_after_purchase(
+        self, mock_lookup, mock_download, mock_enqueue
+    ):
+        filepath = (
+            "recap/gov.uscourts.dcd.178502/gov.uscourts.dcd.178502.2.0_18.pdf"
+        )
+        mock_lookup.return_value = {"filepath_local": filepath}
+        mock_download.return_value = b"\x68\x65\x6c\x6c\x6f"
+
+        process_fetch_webhook_event(self.webhook_event.id)
+        webhook = FilingWebhookEvent.objects.get(id=self.webhook_event.id)
+
+        self.assertEqual(webhook.status, FilingWebhookEvent.SUCCESSFUL)
+        mock_lookup.assert_called_with(self.webhook_event.doc_id)
+        mock_download.assert_called_with(filepath)
+        mock_enqueue.assert_called_with(
+            self.webhook_event, mock_download(), True
+        )
