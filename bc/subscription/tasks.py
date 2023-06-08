16757be2041a93f2c392aefcc64788b243d6f306
Eduardo Rosendo <eduardojra96@gmail.com>
2023-06-08 15:37:38 -0400

    feat(subscription): Tweak the method that enqueue jobs to create a post
    This commits updates the method to reduce the number of DB queries used to check the sponsorship records.

--- bc/subscription/tasks.py
+++ bc/subscription/tasks.py
@@ -80,8 +80,9 @@ def enqueue_posts_for_docket_alert(
         webhook_event.subscription.pk
     ):
         sponsor_message = None
-        if check_sponsor_message and channel.group.sponsorships.all():  # type: ignore
-            sponsorship = channel.group.sponsorships.first()  # type: ignore
+        sponsorships_for_channel = channel.group.sponsorships.all()  # type: ignore
+        if check_sponsor_message and sponsorships_for_channel:
+            sponsorship = sponsorships_for_channel[0]
             sponsor_message = sponsorship.watermark_message
 
         queue.enqueue(
