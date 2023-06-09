e769fd1139c27cc5bf755d680c01235346a63860
Eduardo Rosendo <eduardojra96@gmail.com>
2023-06-08 21:01:59 -0400

    feat(web): Tweak order of the URL patterns

--- bc/web/urls.py
+++ bc/web/urls.py
@@ -43,14 +43,14 @@ urlpatterns = [
         name="little_cases",
     ),
     path(
-        "little-cases/<slug:slug>/",
-        little_cases_details,
-        name="little_cases_detail",
-    ),
-    path(
         "little-cases/suggest-a-bot/",
         ratelimiter_unsafe_5_per_30m(little_cases_suggest_form),
         name="little_cases_suggest_bot",
+    ),
+    path(
+        "little-cases/<slug:slug>/",
+        little_cases_details,
+        name="little_cases_detail",
     ),
     path(
         "collaboration/",
