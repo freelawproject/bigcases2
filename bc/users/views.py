f66f1a7c6b743fa7d9581de4f8c37d031cead0a4
Eduardo Rosendo <eduardojra96@gmail.com>
2023-06-20 23:57:09 -0400

    refactor(user): Add an early exit to the register view

--- bc/users/views.py
+++ bc/users/views.py
@@ -25,72 +25,71 @@ from .utils.email import EmailType, emails
 @sensitive_variables("cd")
 def register(request: HttpRequest) -> HttpResponse:
     """allow only an anonymous user to register"""
-    redirect_to = get_redirect_or_login_url(request, "next")
-    if request.user.is_anonymous:
-        if request.method == "POST":
-            form = RegisterForm(request.POST)
-            consent_form = OptInConsentForm(request.POST)
-
-            if form.is_valid() and consent_form.is_valid():
-                cd = form.cleaned_data
-                user = User.objects.create_user(
-                    cd["username"], cd["email"], cd["password1"]
-                )
-
-                if cd["first_name"]:
-                    user.first_name = cd["first_name"]
-                if cd["last_name"]:
-                    user.last_name = cd["last_name"]
-
-                # Build and assign the activation key
-                user.activation_key = sha1_activation_key(user.username)
-                user.key_expires = now() + timedelta(days=5)
-
-                email: EmailType = emails["confirm_your_new_account"]
-                send_mail(
-                    email["subject"],
-                    email["body"] % (user.username, user.activation_key),
-                    email["from_email"],
-                    [user.email],
-                )
-                email = emails["new_account_created"]
-                send_mail(
-                    email["subject"] % user.username,
-                    email["body"]
-                    % (
-                        user.get_full_name() or "Not provided",
-                        user.email,
-                    ),
-                    email["from_email"],
-                    email["to"],
-                )
-
-                user.save(
-                    update_fields=[
-                        "first_name",
-                        "last_name",
-                        "activation_key",
-                        "key_expires",
-                    ]
-                )
-                query_string = urlencode(
-                    {"next": redirect_to, "email": user.email}
-                )
-                return redirect(
-                    f"{reverse('register_success')}?{query_string}"
-                )
-        else:
-            form = RegisterForm()
-            consent_form = OptInConsentForm()
-        return render(
-            request,
-            "register/register.html",
-            {"form": form, "consent_form": consent_form},
-        )
-    else:
+    if not request.user.is_anonymous:
         # The user is already logged in. Direct them to their settings page as
         # a logical fallback
-        return HttpResponseRedirect(reverse("home"))
+        return HttpResponseRedirect(reverse("homepage"))
+
+    redirect_to = get_redirect_or_login_url(request, "next")
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        consent_form = OptInConsentForm(request.POST)
+
+        if form.is_valid() and consent_form.is_valid():
+            cd = form.cleaned_data
+            user = User.objects.create_user(
+                cd["username"], cd["email"], cd["password1"]
+            )
+
+            if cd["first_name"]:
+                user.first_name = cd["first_name"]
+            if cd["last_name"]:
+                user.last_name = cd["last_name"]
+
+            # Build and assign the activation key
+            user.activation_key = sha1_activation_key(user.username)
+            user.key_expires = now() + timedelta(days=5)
+
+            email: EmailType = emails["confirm_your_new_account"]
+            send_mail(
+                email["subject"],
+                email["body"] % (user.username, user.activation_key),
+                email["from_email"],
+                [user.email],
+            )
+            email = emails["new_account_created"]
+            send_mail(
+                email["subject"] % user.username,
+                email["body"]
+                % (
+                    user.get_full_name() or "Not provided",
+                    user.email,
+                ),
+                email["from_email"],
+                email["to"],
+            )
+
+            user.save(
+                update_fields=[
+                    "first_name",
+                    "last_name",
+                    "activation_key",
+                    "key_expires",
+                ]
+            )
+            query_string = urlencode(
+                {"next": redirect_to, "email": user.email}
+            )
+            return redirect(f"{reverse('register_success')}?{query_string}")
+    else:
+        form = RegisterForm()
+        consent_form = OptInConsentForm()
+
+    return render(
+        request,
+        "register/register.html",
+        {"form": form, "consent_form": consent_form},
+    )
 
 
 def register_success(request: HttpRequest) -> HttpResponse:
