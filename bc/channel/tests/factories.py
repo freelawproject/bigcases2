42bbae3e3bad563e46a3da3541e52b0b7de9a415
Ashley Engelund (weedySeaDragon @ github) <ashley.engelund@gmail.com>
2023-06-23 13:07:01 -0700

    chore(factories) + fake_token() that can be used by Group- and Channel- Factories

--- bc/channel/tests/factories.py
+++ bc/channel/tests/factories.py
@@ -1,9 +1,27 @@
 import factory
+from django.utils.text import slugify
 from factory import SubFactory
 from factory.django import DjangoModelFactory
 from factory.fuzzy import FuzzyChoice
 
 from bc.channel.models import Channel, Group
+from bc.core.utils.tests.base import faker
+
+
+def fake_token(format_str: str = "#############") -> str:
+    """
+    Return a string filled with random letters and numbers that can be used
+    to fake a token. The string is based on the format_str provided.
+
+    Args:
+        format_str (str): format string that faker.pystr_format will use.
+          Default is "#############" (13 characters ('#') long).
+
+    Returns:
+        (str): A string filled with random letters and numbers
+
+    """
+    return faker.pystr_format(f"{format_str}{{{{random_letter}}}}")
 
 
 class GroupFactory(DjangoModelFactory):
