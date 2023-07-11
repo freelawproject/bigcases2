e47b2bd3b296f8da0e3dfd5f4a7fd30528e2d743
Ashley Engelund (weedySeaDragon @ github) <ashley.engelund@gmail.com>
2023-07-11 10:11:44 -0700

    remove unused CommandUtils; add VerboseCommand doc comment

--- bc/core/management/commands/command_utils.py
+++ bc/core/management/commands/command_utils.py
@@ -1,12 +1,13 @@
 # This file is a slightly modified copy from CourtListener
 
 import logging
-import os
 
-from django.core.management import BaseCommand, CommandError
+from django.core.management import BaseCommand
 
 
 class VerboseCommand(BaseCommand):
+    """Add logging based on the verbosity argument."""
+
     logger = logging.getLogger(__name__)
 
     def handle(self, *args, **options):
@@ -19,15 +20,3 @@ class VerboseCommand(BaseCommand):
             self.logger.setLevel(logging.INFO)
         elif verbosity > 1:
             self.logger.setLevel(logging.DEBUG)
-
-
-class CommandUtils:
-    """A mixin to give some useful methods to subclasses."""
-
-    @staticmethod
-    def ensure_file_ok(file_path):
-        """Check to make sure that a file path exists and is valid."""
-        if not os.path.exists(file_path):
-            raise CommandError(f"Unable to find file at {file_path}")
-        if not os.access(file_path, os.R_OK):
-            raise CommandError(f"Unable to read file at {file_path}")
