8d7b5e0b2352baf9512d4ca51639bbbad8eeba55
Elisa Anguita <elisa@free.law>
2024-10-31 11:21:30 -0300

    fix(settings): Fix default value for DOCTOR_HOST env variable

--- bc/settings/misc.py
+++ bc/settings/misc.py
@@ -5,4 +5,4 @@ env = environ.FileAwareEnv()
 # Numbers of seconds the app should wait to process a webhook
 WEBHOOK_DELAY_TIME = env.int("WEBHOOK_DELAY_TIME", default=120)
 
-DOCTOR_HOST = env("DOCTOR_HOST", default="http://bc-doctor:5050")
+DOCTOR_HOST = env("DOCTOR_HOST", default="http://bc2-doctor:5050")
