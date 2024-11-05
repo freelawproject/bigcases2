a7cb8960c013bfbefe260255c168b192a2dbabbb
Elisa Anguita <elisa@free.law>
2024-11-05 17:50:13 -0300

    feat(Threads): Refresh Threads access tokens automatically.

--- bc/channel/signals.py
+++ bc/channel/signals.py
@@ -1,3 +1,6 @@
+import logging
+from datetime import timedelta
+
 from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -8,8 +11,11 @@ from rq import Retry
 from bc.core.utils.cloudfront import create_cache_invalidation
 
 from .models import Channel, Group
+from .tasks import refresh_threads_access_token
 
 queue = get_queue("default")
+
+logger = logging.getLogger(__name__)
 
 
 @receiver(post_save, sender=Group)
@@ -48,4 +54,19 @@ def channel_handler(sender, instance=None, created=False, **kwargs):
                 max=settings.RQ_MAX_NUMBER_OF_RETRIES,
                 interval=settings.RQ_RETRY_INTERVAL,
             ),
+        )
+
+    # Schedule initial token refresh 2 days after creation
+    if created and instance.service == Channel.THREADS:
+        queue.enqueue_in(
+            timedelta(days=2),
+            refresh_threads_access_token,
+            channel_pk=instance.pk,
+            retry=Retry(
+                max=settings.RQ_MAX_NUMBER_OF_RETRIES,
+                interval=settings.RQ_RETRY_INTERVAL,
+            ),
+        )
+        logger.info(
+            f"Scheduled new refresh token for newly created channel {instance} in 2 days"
         )
