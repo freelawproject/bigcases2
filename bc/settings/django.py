ada029f2e8188e14209e18b25c3340bc74a2435a
Elisa Anguita <elisa@free.law>
2024-12-06 13:54:41 -0300

    refactor(settings): remove deprecated code

--- bc/settings/django.py
+++ bc/settings/django.py
@@ -81,7 +81,6 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "bc.core.utils.context_processors.banner_settings",
             ],
         },
     },
