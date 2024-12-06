2eb01fc40ce755c58c6347a70a7e066970023b7e
Elisa Anguita <elisa@free.law>
2024-12-06 14:37:37 -0300

    feat(banner_config): enable filters and display additional fields in admin

--- bc/core/admin.py
+++ bc/core/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 
 from bc.core.models import BannerConfig
 
-admin.site.register(BannerConfig)
+
+@admin.register(BannerConfig)
+class BannerConfigAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "is_active", "banner_title", "banner_text")
+    list_filter = ("is_active",)
+    search_fields = (
+        "banner_title",
+        "banner_text",
+        "banner_button_text",
+        "banner_button_link",
+    )
